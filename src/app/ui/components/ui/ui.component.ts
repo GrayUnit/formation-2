895a07cceed254e4b83f6dbfacdaad0c8b40acde
GrayUnit <grayunit@outlook.fr>
2020-09-28 14:38:23 +0200

    Création du composant UI et du premier icon

---
+++ src/app/ui/components/ui/ui.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.scss']
+})
+export class UiComponent implements OnInit {
+
+  public title: string;
+  public menuIsOpen: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.title = "Bienvenue";
+    this.menuIsOpen = false;
+  }
+
+  public toggleMenu() {
+    this.menuIsOpen = !this.menuIsOpen;
+  }
+
+}
