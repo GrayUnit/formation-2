c97b41ebcb10f025d8a1e6951b834718f67a7869
GrayUnit <grayunit@outlook.fr>
2020-09-30 10:19:29 +0200

    Utilisation du pipe async dans le module order

--- src/app/orders/pages/page-list-order/page-list-order.component.ts
+++ src/app/orders/pages/page-list-order/page-list-order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
 import { StateOrder } from '../../enums/state-order.enum';
 import { OrdersService } from '../../services/orders.service';
@@ -8,20 +9,23 @@ import { OrdersService } from '../../services/orders.service';
   templateUrl: './page-list-order.component.html',
   styleUrls: ['./page-list-order.component.scss']
 })
-export class PageListOrderComponent implements OnInit {
+export class PageListOrderComponent implements OnInit, OnDestroy {
 
-  public ordersList: Order[];
+  public orders: Order[];
+  public collectionOrders$: Observable<Order[]>;
   public tableHeaders: string[];
   public states = Object.values(StateOrder);
+  public subscription: Subscription;
 
   constructor(private orderService: OrdersService) { }
 
   ngOnInit(): void {
-    this.orderService.collection.subscribe(
-      (datas) => {
-        this.ordersList = datas;
-      }
-    );
+    // this.subscription = this.orderService.collection.subscribe(
+    //   (datas) => {
+    //     this.orders = datas;
+    //   }
+    // );
+    this.collectionOrders$ = this.orderService.collection;
     this.tableHeaders = [
       "Type",
       "Client",
@@ -31,6 +35,10 @@ export class PageListOrderComponent implements OnInit {
       "Total TTC",
       "State"
     ]
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   public changeState(item: Order, event) {
